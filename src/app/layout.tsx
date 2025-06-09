c36a923113a5106322b7f3446c0ad0efe34cfc8e
Leonardo Braga <programer_frontbraga@outlook.com>
2025-06-09 12:41:35 -0300

    feat: define inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import { inter } from '@/lib/fonts/fonts';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <html lang='en'>
-      <body className='antialiased'>
-        {children}
-      </body>
-    </html>
-  )
-}
+	return (
+		<html lang="en">
+			<body className={`${inter.className} antialiased`}>{children}</body>
+		</html>
+	);
+};
 
-export default RootLayout
+export default RootLayout;
